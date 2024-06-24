1705b62e8dce1b2a5b584a7e206664d534ee267e
klvallejo1 <leandrohernandez1903@gmail.com>
2024-06-24 13:24:39 -0500

    NavBar agregada

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,14 +4,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MaterialModule } from './modules/material/material.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [
     provideClientHydration(),
---
+++ frontend/src/app/modules/material/material.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
+const material=[
+  MatToolbarModule,
+  MatButtonModule
+];
+
+@NgModule({
+  declarations: [],
+  imports: [material],
+  exports: [
+    material
+  ]
+})
+export class MaterialModule { }
